f6303621025f15b1211da7d85699f55a5321e68c
nuzattasnim <nuzat.tasnim@selisegroup.com>
2025-12-12 01:06:25 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// middleware to parse JSON
+app.use(express.json());
+app.use(cors());
+
+// simple API route
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Express API!" });
+});
+
+// optional: a POST example
+app.post("/api/echo", (req, res) => {
+  res.json({ you_sent: req.body });
+});
+
+// start server
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
